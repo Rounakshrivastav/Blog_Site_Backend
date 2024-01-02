00233578e02aae8f0f25bb851c79059ff223dc0e
Rounak Shrivastav <shrivastavrounak6@gmail.com>
2024-01-02 20:22:45 +0530

    Backend API

---
+++ utils/feature.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken"
+
+export const generateCookie = (user,res,statusCode=200,message)=>{
+    const token = jwt.sign({_id:user._id},process.env.JWT_SECRET)
+    
+    // console.log(token)
+
+    res.status(201).cookie("token",token,{
+        httpOnly:true,
+        maxAge:10*60*1000,
+        sameSite:process.env.NODE_ENV === "Develpoment" ? "lax":"none",
+        secure:process.env.NODE_ENV === "Develpoment"?false:true
+    }).json({
+        success:true,
+        message
+    })
+}
